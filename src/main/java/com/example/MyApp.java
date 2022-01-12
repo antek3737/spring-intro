9e56cf7f6c648ea64037d0e1982d6762aeb35d93
antek3737 <antonizub00@gmail.com>
2022-01-12 19:29:57 +0100

    zrobione 2

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -1,16 +1,17 @@
 package com.example;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyApp {
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(Config.class);
+        MessageService messageService = applicationContext.getBean(MyNameMessageService.class);
+        MessageService messageService2 = applicationContext.getBean(RandomTextMessageService.class);
 
-        MessageService messageService = applicationContext.getBean("messageService", MessageService.class);
-
-        System.out.println(messageService.getMessage());
-
-        applicationContext.close();
+        System.out.println(messageService.getMessage().hashCode());
+        System.out.println(messageService2.getMessage().hashCode());
     }
 }
